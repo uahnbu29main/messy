1c6da1bfddfec84c7df3e512510ef44bcd60ceaa
uahnbu29main <uahnbu29main@gmail.com>
2020-07-18 20:19:52 +0700

    Add files via upload

---
+++ build/precache-manifest.e0d6b12ffb19dde1489a44fb4e269971.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3144109727a94ef84ff6b6f72972d67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc2171d595cca007a59f",
+    "url": "/static/css/main.66568c07.chunk.css"
+  },
+  {
+    "revision": "f65ad9cfb24c5c585a4c",
+    "url": "/static/js/2.f3794765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f3794765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc2171d595cca007a59f",
+    "url": "/static/js/main.4fec2214.chunk.js"
+  },
+  {
+    "revision": "7f889ae15e9bbd51da2c",
+    "url": "/static/js/runtime-main.71b06475.js"
+  }
+]);
